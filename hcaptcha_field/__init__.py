56bded0dce4725999d9fa583752e33baa651fdfb
Ties Jan Hefting <tiesjan@smart.pr>
2021-06-07 21:55:47 +0200

    Add packaging configuration
    A GitHub action is included to automatically publish packages when a new
    tag is created.

--- hcaptcha_field/__init__.py
+++ hcaptcha_field/__init__.py
@@ -1,6 +1,12 @@
+import django
+
 from .fields import hCaptchaField
 from .settings import hcaptcha_settings
 from .widgets import hCaptchaWidget
 
 
 __all__ = ('hCaptchaField', 'hCaptchaWidget', 'hcaptcha_settings')
+
+
+if django.VERSION < (3, 2):
+    default_app_config = 'hcaptcha_field.apps.hCaptchaFieldConfig'
--- setup.py
+++ setup.py
@@ -1,3 +1,48 @@
-from setuptools import setup
+import setuptools
 
-setup()
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+
+setuptools.setup(
+    # Metadata
+    name='django-hcaptcha-field',
+    version='0.9',
+    license='BSD',
+    author='Ties Jan Hefting',
+    author_email='hello@tiesjan.com',
+    description='Django hCaptcha Field provides a simple way to protect your Django forms using hCaptcha.',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/tiesjan/django-hcaptcha-field',
+    project_urls={
+        'Bugs': 'https://github.com/tiesjan/django-hcaptcha-field/issues',
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+
+    # Options
+    packages=setuptools.find_packages(exclude=('py_tests*',)),
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.5',
+    install_requires=(
+        'Django>=2.2,<=4.0',
+    )
+)
