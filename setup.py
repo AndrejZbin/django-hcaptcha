48d79af85af8f650e3d08d6c7d370dc4758304b8
Ties Jan Hefting <hello@tiesjan.com>
2022-02-08 16:18:44 +0100

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 setuptools.setup(
     # Metadata
     name='django-hcaptcha-field',
-    version='1.2.0',
+    version='1.2.1',
     license='BSD',
     author='Ties Jan Hefting',
     author_email='hello@tiesjan.com',
